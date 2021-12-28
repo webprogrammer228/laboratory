e8561bde5240091a3376efd24f7bf86781baf06d
Bogdan Gordeychuk <comedi4@gmail.com>
2021-12-28 14:55:27 +0600

    new edit styles

--- src/components/SecondScreen/Popular-mobile.js
+++ src/components/SecondScreen/Popular-mobile.js
@@ -1,7 +1,5 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
 import "../../assets/scss/secondScreen/popularMobile.scss";
 import { useEffect } from "react";
 
