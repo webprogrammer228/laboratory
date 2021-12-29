991f3f5733e68da3ce132c6a36f897532973ca12
Bogdan Gordeychuk <comedi4@gmail.com>
2021-12-29 13:31:00 +0600

    pravki

--- src/components/Header/Topinfo.js
+++ src/components/Header/Topinfo.js
@@ -6,6 +6,37 @@ import '../../assets/scss/header/top.scss';
 import { Link } from "react-router-dom";
 
 function Topinfo() {
+
+  window.onscroll = function() {
+    var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if(scrolled >= 80) {
+      document.querySelector(".top-info").style.visibility = 'hidden';
+      document.querySelector(".top-info").style.height = '0';
+      document.querySelector(".top-info").style.marginTop = '-10px';
+      document.querySelector(".top-info").style.transition = 'all .3s';
+      
+      document.querySelector(".bottom_info").style.visibility = 'hidden';
+      document.querySelector(".bottom_info").style.height = '0';
+      document.querySelector(".bottom_info").style.marginTop = '-150px';
+      document.querySelector(".bottom_info").style.transition = 'all .5s';
+
+
+      document.querySelector(".lab-header").style.height = '96px';
+    }
+    else {
+      document.querySelector(".top-info").style.marginTop = '7px';
+      document.querySelector(".top-info").style.height = '40px';
+      document.querySelector(".top-info").style.visibility = 'visible';
+
+      document.querySelector(".bottom_info").style.marginTop = '0px';
+      document.querySelector(".bottom_info").style.height = '80px';
+      document.querySelector(".bottom_info").style.visibility = 'visible';
+
+      document.querySelector(".lab-header").style.height = '208px';
+    }
+  }
+
+
   return (
     <>
       <div className="top-info">
